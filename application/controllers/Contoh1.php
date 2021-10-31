2a4321c1bf7e95edded331b11d13d59c03a64546
shalliaa <shalliaayuthahirakum@gmail.com>
2021-10-31 15:34:39 +0700

    menambahkan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Shallia Yuthahirakum
+ Saya tingga di daerah Tasikmalaya
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
